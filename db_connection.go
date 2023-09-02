161cdf64b4db92ba98e4d0d79f45b03f1d8ea0b1
salah-jr <mohammed.s.korna@gmail.com>
2023-09-02 22:59:19 +0200

    Rename files

--- db_connection.go
+++ db_connection.go
@@ -9,8 +9,10 @@ import (
 func InitDb() *gorm.DB {
 	dsn := "root:@/crud?charset=utf8mb4&parseTime=True&loc=Local"
 	Db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
 	if err != nil {
 		log.Fatal("Error connecting database")
 	}
+
 	return Db
 }
